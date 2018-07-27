90b6b9cb8cdc5e866f77995dd144ea54f034d9bf
Rayhan Chowdhury <rayhan379@users.noreply.github.com>
2018-07-27 16:30:19 +0600

    Add files via upload

---
+++ FibonacciSeries.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+
+public class FibonacciSeries {
+
+	public static void main(String[] args) {
+		Scanner input=new Scanner(System.in);
+		 System.out.print("How many Numbers =");
+		 int n=input.nextInt();
+		 int first=0;
+		 int second=1;
+		 int fibo;
+		 for(int i=0;i<=n;i++) 
+		 {
+			 fibo=first+second;
+			 System.out.print(fibo+" ");
+			 first=second;
+			 second=fibo;
+		 }
+
+	}
+
+}
